5c042152be9a3381d51f36f0c75c287ea4b8266d
Robert Rollner <robrollner@gmail.com>
2017-10-19 17:51:41 -0600

    updated friends route

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,10 +1,10 @@
 const friends = require('../app/data/friends');
 
 module.exports = (app) => {
-    app.get('/api/friends', (req, res) => {
+    app.get('/api/data/friends', (req, res) => {
         res.json(friends);
     })
-    app.post('/api/friends', (req, res) => {
+    app.post('/api/data/friends', (req, res) => {
         friends.push(req.body);
     })
 
