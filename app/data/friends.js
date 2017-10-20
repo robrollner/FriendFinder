9b979e52867e7f629a449472e87d89a3a7990197
Robert Rollner <robrollner@gmail.com>
2017-10-19 20:12:01 -0600

    damn required file to start

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+const apiRoutes = require('../../routing/apiRoutes');
+const htmlRoutes = require('../../routing/htmlRoutes');
+const server = require('../../server');
+
+var friendNumber = 100;
+
+function friendNumberAdd() {
+
+}
