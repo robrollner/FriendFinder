629349d91158f0385dbf230318fb6a0716356e1e
Robert Rollner <robrollner@gmail.com>
2017-10-22 17:11:31 -0600

    game logic isn't showing modal, but name posts to JSON file

--- survey.js
+++ survey.js
@@ -1,108 +1,113 @@
-let newFriend = {};
-let bestFriend = {
-    diff: 99,
-    name: '',
-    image: ''
-};
+$(document).ready(function() {
+    var newFriend = {};
+    var bestFriend = {
+        compare: 666,
+        name: "",
+        image: "",
+    };
 
-$('#submit').on('click', () => {
-    var corgiLover = $('#name').val().trim();
-    var pic = $('#image').val().trim();
-    var ans1 = $('#quest1').val().trim();
-    var ans2 = $('#quest2').val().trim();
-    var ans3 = $('#quest3').val().trim();
-    var ans4 = $('#quest4').val().trim();
-    var ans5 = $('#quest5').val().trim()
-    var ans6 = $('#quest6').val().trim();
-    var ans7 = $('#quest7').val().trim();
-    var ans8 = $('#quest8').val().trim();
-    var ans9 = $('#quest9').val().trim();
-    var ans10 = $('#quest10').val().trim();
 
-    if (corgiLover === "" || pic === "" || ans1 === "" || ans2 === "" || ans3 === "" || ans4 === "" || ans5 === "" || ans6 === "" || ans7 === "" || ans8 === "" || ans9 === "" || ans10 === "") {
-        alert('Faceless men do not love Corgis, please provide your name and picture')
-    } else {
-        newFriend = {
-            name: corgiLover,
-            photo: pic,
-            scores: [ans1,
-                ans2,
-                ans3,
-                ans4,
-                ans5,
-                ans6,
-                ans7,
-                ans8,
-                ans9,
-                ans10
-            ]
+
+    $("#submitBtn").on("click", function() {
+        let name = $("#name").val().trim();
+        let image = $("#image").val().trim();
+        let q1 = $("#q1").val().trim();
+        let q2 = $("#q2").val().trim();
+        let q3 = $("#q3").val().trim();
+        let q4 = $("#q4").val().trim();
+        let q5 = $("#q5").val().trim();
+        let q6 = $("#q6").val().trim();
+        let q7 = $("#q7").val().trim();
+        let q8 = $("#q8").val().trim();
+        let q9 = $("#q9").val().trim();
+        let q10 = $("#q10").val().trim();
+        if (name === "" ||
+            image === "" ||
+            q1 === "" ||
+            q2 === "" ||
+            q3 === "" ||
+            q4 === "" ||
+            q5 === "" ||
+            q6 === "" ||
+            q7 === "" ||
+            q8 === "" ||
+            q9 === "" ||
+            q10 === "") {
+
+            alert('Faceless men do not love Corgis, please provide your name and picture')
+
+        } else {
+
+            newFriend = {
+                name: name,
+                image: image,
+                answers: [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]
+            }
+        };
+
+        friendSurvey(newFriend.answers);
+
+        setTimeout(postData, 3000);
+
+        function postData() {
+            $.post({ url: '/api/friends', contentType: 'application/json' }, JSON.stringify(newFriend));
         }
 
-        friendSurvey(corgiLover.scores);
+
+        $("#name").val("");
+        $("#image").val("");
+        $("#q1").val("");
+        $("#q2").val("");
+        $("#q3").val("");
+        $("#q4").val("");
+        $("#q5").val("");
+        $("#q6").val("");
+        $("#q7").val("");
+        $("#q8").val("");
+        $("#q9").val("");
+        $("#q10").val("");
+
+    })
 
 
-        setTimeout(updateData, 750);
-
-        function updateData() {
-            $.post({ url: '/api/friends', contentType: 'application/json' }, JSON.stringify(corgiLover));
-        }
-
-        $('#name').val('');
-        $('#image').val('');
-        $('#quest1').val('');
-        $('#quest2').val('');
-        $('#quest3').val('');
-        $('#quest4').val('');
-        $('#quest5').val('');
-        $('#quest6').val('');
-        $('#quest7').val('');
-        $('#quest8').val('');
-        $('#quest9').val('');
-        $('#quest10').val('');
-
-    }
-
-    // console.log(this);
-    // updateData();
 })
 
-function friendSurvey(scores) {
+
+function friendSurvey(answers) {
 
     $.get('/api/friends', (friends) => {
-        var number = 0;
+        var count = 0;
         var friendArray = friends.length;
 
         for (var i = 0; i < friendArray.length; i++) {
-            friendScore(scores, friends[i]);
-            number++;
+            friendCompare(answers, friends[i]);
+            count++;
         }
 
-        if (number === friendArray) {
-            $('#corgiLover').text(bestFriend.name);
-            $('#corgiImg').attr('src', bestFriend.image);
+        if (count === friendArray) {
             $('#myModal').modal('toggle');
+            $('#corgiLover').html(bestFriend.name + " loves Corgis too!");
+            $('#corgiImg').attr('src', bestFriend.image);
 
         }
-    });
+    })
 }
 
-function friendScore(user, friend) {
-
-    var diff = 0;
-    var number = 0;
+function friendCompare(user, friend) {
+    var compare = 0;
+    var count = 0;
 
     for (var i = 0; i < 5; i++) {
-        diff += Math.abs(user[i] - friend.scores[i]);
-        number++;
+        compare += Math.abs(user[i] - friend.answers[i]);
+        count++;
     }
 
-    if (number === 5) {
-        if (diff < bestFriend.diff) {
-            bestFriend.diff = diff;
+    if (count === 5) {
+        if (compare < bestFriend.compare) {
+            bestFriend.compare = compare;
             bestFriend.name = friend.name;
-            bestFriend.image = friend.photo;
+            bestFriend.image = friend.image;
         } else {
-
             return;
         }
     }
